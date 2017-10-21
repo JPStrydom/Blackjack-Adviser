79e8ffc2dec27cfc8bbeced95f08d06c6da031b5
STARDESTROYER\JP <jpstrydom8@gmail.com>
2017-10-21 13:29:43 +0200

    Update production fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
     entry: ['./src/index.js'],
     output: {
@@ -18,5 +20,10 @@ module.exports = {
     },
     devServer: {
         contentBase: './'
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }),
+    ]
 };
